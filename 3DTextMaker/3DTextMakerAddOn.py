394da14b437abb2b2f3cafe624dfecd3cdd98ce4
dmiyamo3 <mydeveloper2@gmail.com>
2022-04-12 01:04:17 +0900

    保存時のファイル名にオプションを追加

--- 3DTextMaker/3DTextMakerAddOn.py
+++ 3DTextMaker/3DTextMakerAddOn.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import bpy
 from bpy.props import *
@@ -15,8 +16,9 @@ bl_info = {
     "location": "",
     "warning": "",
     "wiki_url": "",
-    "tracker_url":""
+    "tracker_url": ""
 }
+
 
 def makesplittext(i, text, fontpath, extrude, offset, bevel_depth, bevel_resolution):
     bpy.ops.object.text_add(enter_editmode=False, location=(0, 0, 0))
@@ -40,13 +42,20 @@ def makesplittext(i, text, fontpath, extrude, offset, bevel_depth, bevel_resolut
     bpy.context.object.data.bevel_resolution = bevel_resolution
 
 
-def savetext(i,text, dirname):
+def savetext(i, text, dirname):
     # save fbx
     # dirname = dirdialog()
     filename = os.path.join(dirname, str(i) + "_" + text + ".fbx")
     bpy.ops.export_scene.fbx(filepath=filename, use_selection=True)
 
-### tkinterがblenderと競合(?)して使えないのでファイルダイアログは一旦保留
+
+def savetext2(i, text, dirname):
+    # save fbx
+    # dirname = dirdialog()
+    filename = os.path.join(dirname, text + "_" + str(i) + ".fbx")
+    bpy.ops.export_scene.fbx(filepath=filename, use_selection=True)
+
+# tkinterがblenderと競合(?)して使えないのでファイルダイアログは一旦保留
 # def dirdialog():
 #     root = tkinter.Tk()
 #     root.withdraw()
@@ -66,8 +75,8 @@ class TUTORIAL_OT_3DTextMaker(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     #--- properties ---#
-    text:       StringProperty(default = "", options = {"HIDDEN"})
-    fontpath:   StringProperty(default = "", options = {"HIDDEN"})
+    text:       StringProperty(default="", options={"HIDDEN"})
+    fontpath:   StringProperty(default="", options={"HIDDEN"})
     extrude:    FloatProperty()
     offset:     FloatProperty()
     bevel_depth:        FloatProperty()
@@ -75,10 +84,11 @@ class TUTORIAL_OT_3DTextMaker(bpy.types.Operator):
 
     #--- execute ---#
     def execute(self, context):
-        for i,c in enumerate(self.text):
-            makesplittext(i, c, self.fontpath, self.extrude, self.offset, self.bevel_depth, self.bevel_resolution)
+        for i, c in enumerate(self.text):
+            makesplittext(i, c, self.fontpath, self.extrude,
+                          self.offset, self.bevel_depth, self.bevel_resolution)
 
-        for i,c in enumerate(bpy.context.scene.text):
+        for i, c in enumerate(bpy.context.scene.text):
             bpy.data.objects[str(i) + "_" + c].location.x = i * 1.0
 
         return {'FINISHED'}
@@ -86,22 +96,28 @@ class TUTORIAL_OT_3DTextMaker(bpy.types.Operator):
 #
 # TUTORIAL_OT_SaveText
 #
+
+
 class TUTORIAL_OT_SaveText(bpy.types.Operator):
     bl_idname = "tutorial.savetext"
     bl_label = "savetext"
     bl_options = {'REGISTER', 'UNDO'}
 
     #--- properties ---#
-    dirname: StringProperty(default = "", options ={"HIDDEN"})
+    dirname: StringProperty(default="", options={"HIDDEN"})
+    save_option_full_text: BoolProperty(name="チェック", default=True)
 
     #--- execute ---#
     def execute(self, context):
         for obj in bpy.context.selected_objects:
             obj.select_set(False)
 
-        for i,c in enumerate(bpy.context.scene.text):
+        for i, c in enumerate(bpy.context.scene.text):
             bpy.data.objects[str(i) + "_" + c].select_set(True)
-            savetext(i, c, self.dirname)
+            if self.save_option_full_text is True:
+                savetext2(i, bpy.context.scene.text, self.dirname)
+            else:
+                savetext(i, c, self.dirname)
             bpy.data.objects[str(i) + "_" + c].select_set(False)
 
         return {'FINISHED'}
@@ -129,7 +145,8 @@ class TUTORIAL_PT_3DTextMakerPanel(bpy.types.Panel):
         layout.prop(context.scene, "bevel_depth")
         layout.prop(context.scene, "bevel_resolution")
 
-        on_prop = layout.operator(TUTORIAL_OT_3DTextMaker.bl_idname, text = "Create")
+        on_prop = layout.operator(
+            TUTORIAL_OT_3DTextMaker.bl_idname, text="Create")
         on_prop.text = context.scene.text
         on_prop.fontpath = context.scene.fontpath
         on_prop.extrude = context.scene.extrude
@@ -138,9 +155,13 @@ class TUTORIAL_PT_3DTextMakerPanel(bpy.types.Panel):
         on_prop.bevel_resolution = context.scene.bevel_resolution
 
         layout.prop(context.scene, "dirname")
+        layout.prop(context.scene, "save_option_full_text")
 
-        on_prop_save = layout.operator(TUTORIAL_OT_SaveText.bl_idname, text = "Save")
+        on_prop_save = layout.operator(
+            TUTORIAL_OT_SaveText.bl_idname, text="Save")
         on_prop_save.dirname = context.scene.dirname
+        on_prop_save.save_option_full_text = context.scene.save_option_full_text
+
 
 #
 # register classs
@@ -151,18 +172,20 @@ classes = [
     TUTORIAL_PT_3DTextMakerPanel
 ]
 
+
 def register():
     for c in classes:
         bpy.utils.register_class(c)
 
     # Panel用変数の登録
-    bpy.types.Scene.text = StringProperty(default = "")
-    bpy.types.Scene.dirname = StringProperty(default = "")
-    bpy.types.Scene.fontpath = StringProperty(default = "")
+    bpy.types.Scene.text = StringProperty(default="")
+    bpy.types.Scene.dirname = StringProperty(default="")
+    bpy.types.Scene.fontpath = StringProperty(default="")
     bpy.types.Scene.extrude = FloatProperty()
     bpy.types.Scene.offset = FloatProperty()
     bpy.types.Scene.bevel_depth = FloatProperty()
     bpy.types.Scene.bevel_resolution = IntProperty()
+    bpy.types.Scene.save_option_full_text = BoolProperty(default=True)
 
 
 def unregister():
@@ -177,6 +200,7 @@ def unregister():
     del bpy.types.Scene.offset
     del bpy.types.Scene.bevel_depth
     del bpy.types.Scene.bevel_resolution
+    del bpy.types.Scene.save_option_full_text
 
 
 if __name__ == "__main__":
